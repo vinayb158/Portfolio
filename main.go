6e1696a7148bb89140a52c2764f54bd161d2946d
vinayb158 <vinayb158@gmail.com>
2025-03-11 09:50:31 +0530

    Copnnection string changed

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	DbConn := os.Getenv("Portfolio_DB_Connection")
+	//DbConn := os.Getenv("Portfolio_DB_Connection")
+
+	DbConn := "postgres://postgres:@smcltd12@localhost:5432/postgres"
+
 	if DbConn == "" {
 		fmt.Println("DB connection string is missing!")
 		return
@@ -18,30 +20,30 @@ func main() {
 
 	fmt.Println("Using DB Connection:", DbConn)
 	conn, err := pgx.Connect(context.Background(), DbConn)
-    if err != nil {
-        log.Fatalf("Unable to connect to database: %v\n", err)
-    }
-    defer conn.Close(context.Background())
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Close(context.Background())
 
-    // Run a simple query
-    rows, err := conn.Query(context.Background(), "SELECT party,secu FROM porttrxn")
-    if err != nil {
-        log.Fatalf("Query failed: %v\n", err)
-    }
-    defer rows.Close()
+	// Run a simple query
+	rows, err := conn.Query(context.Background(), "SELECT party,secu FROM porttrxn")
+	if err != nil {
+		log.Fatalf("Query failed: %v\n", err)
+	}
+	defer rows.Close()
 
-    // Process the result
-    for rows.Next() {
-        var party string
-        var secu string
-        if err := rows.Scan(&party, &secu); err != nil {
-            log.Fatalf("Row scan failed: %v\n", err)
-        }
+	// Process the result
+	for rows.Next() {
+		var party string
+		var secu string
+		if err := rows.Scan(&party, &secu); err != nil {
+			log.Fatalf("Row scan failed: %v\n", err)
+		}
 
-        fmt.Printf("Party: %s, Scrip: %s\n", party, secu)
-    }
-    // Check if there were any errors in the row iteration
-    if err := rows.Err(); err != nil {
-        log.Fatalf("Rows iteration failed: %v\n", err)
-    }
+		fmt.Printf("Party: %s, Scrip: %s\n", party, secu)
+	}
+	// Check if there were any errors in the row iteration
+	if err := rows.Err(); err != nil {
+		log.Fatalf("Rows iteration failed: %v\n", err)
+	}
 }
